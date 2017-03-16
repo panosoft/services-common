fc601b37553fabc2bccdc0cae159bdb4b4b86c29
Charles Scalfani <charles@panosoft.com>
2017-03-16 14:52:15 -0700

    Added SessionNotFound error message

---
+++ src/Services/Common/Error.elm
@@ -0,0 +1,17 @@
+module Services.Common.Error
+    exposing
+        ( sessionNotFound
+        )
+
+{-|
+    Common Services errors.
+
+@docs sessionNotFound
+-}
+
+
+{-| Session not found error message.
+-}
+sessionNotFound : String
+sessionNotFound =
+    "SessionNotFound:b06f3667-3e35-42ce-b7e4-9a5a2cf23251"
